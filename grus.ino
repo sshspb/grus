645cabf9fcb179d40ac4487fde92c38fe3580593
shs <ssh.spb@gmail.com>
2018-12-07 09:19:38 +0300

    increase reliability

--- grus.ino
+++ grus.ino
@@ -1,9 +1,6 @@
-//#include <SoftwareSerial.h>
 #include <OneWire.h>
 #include <LCDi2cW.h>                    
 
-//#define RX_UNO 7
-//#define TX_UNO 8
 #define ONE_WIRE_UNO 2
 #define PWR_KEY_UNO 11
 #define PWR_LED_UNO 12
@@ -20,9 +17,8 @@
 #define DEVICE_COUNT 4
 #define USERS_COUNT 3
 #define MODEM_RATE 115200L
-//#define MODEM_RATE 9600L
 
-const char* comrade[USERS_COUNT] = {"+79219258698", "+79214201935", "+79213303129"};
+const char* comrade[USERS_COUNT] = {"+7ХХХХХХХХХХ", "+7ХХХХХХХХХХ", "+7ХХХХХХХХХХ"};
 const byte deviceAddress[DEVICE_COUNT][8]  = {
   { 0x28, 0x6C, 0x8F, 0x53, 0x03, 0x00, 0x00, 0xB0 }, // #1 Sensor  0m grey hub
   { 0x28, 0xF9, 0xCD, 0x53, 0x03, 0x00, 0x00, 0x80 }, // #2 Sensor 15m white hub
@@ -32,21 +28,24 @@ const byte deviceAddress[DEVICE_COUNT][8]  = {
 const int temp_null = 1598;  // (int) 99,9 * 16
 int currentTemperature[DEVICE_COUNT];
 int hourlyTemperature[DEVICE_COUNT][24];
-unsigned long reportTime, measureTime, messageTime, timeout;
+unsigned long measureTime, messageTime, timeout;
 char creg1 = '?', creg2 = '?', creg3 = '?';
 char buff[BUFF_SIZE];
 char answer[DEVICE_COUNT+1][23];
 bool isEven = true;
 byte connected = 0;
 
-//SoftwareSerial modem(RX_UNO, TX_UNO);
 OneWire ds(ONE_WIRE_UNO);
 LCDi2cW lcd = LCDi2cW(4,20,0x4C,0);
 
 void getSignalQuality () {
+  bool isReq = false;
+  timeout = millis() + 1000L;
+  while (Serial.available() > 0 && millis() < timeout) {
+    Serial.read();
+  }
   Serial.write("AT+CSQ"); 
   Serial.write(GSM_CR);
-  bool isReq = false;
   timeout = millis() + 1000L;
   do {
     if (Serial.available()) {
@@ -59,8 +58,8 @@ void getSignalQuality () {
   } while (millis() < timeout);
   char csqm = isReq ? buff[6] : '?';
   char csql = isReq ? buff[7] : '?';
-  //sprintf(buff, "SIGNAL Q:%c%c R:%c,%c%c", csqm, csql, creg1, creg2, creg3);
-  sprintf(buff, "SIGNAL Q:%c%c", csqm, csql);
+  if (creg3 == ' ') sprintf(buff, "SIGNAL Q:%c%c", csqm, csql);
+  else sprintf(buff, "SIGNAL Q:%c%c R:%c,%c%c", csqm, csql, creg1, creg2, creg3);
   lcd.setCursor(0, 0);
   lcd.print(buff);
   sprintf(answer[0], "%s", buff);
@@ -103,8 +102,7 @@ void getTemp() {
       }
       sprintf(buff, "%d: %3d,  min %3d", s+1, (int) floor(currentTemperature[s]/16), (int) floor(tmin/16) );
       sprintf(answer[row], "\n%s", buff);
-      // на случай если больше трёх датчиков подключено
-      // на дисплей первые три датчика / последние три
+      // на случай если больше трёх датчиков подключено отображаем первые три датчика / последние три
       if (++countDev > firstDev && row < 4) {
         lcd.setCursor(row++, 0);
         lcd.print(buff);
@@ -124,13 +122,15 @@ void getTemp() {
 bool performModem(const char* command, byte mode, byte retry, byte waitok) {
   byte countok = 0;
   bool resOK = false;
+  timeout = millis() + 1000L;
+  while (Serial.available() > 0 && millis() < timeout) {
+    Serial.read();
+  }
   for (byte i = 0; i < retry; i++) {
-//    lcd.setCursor(0,19);
-//    lcd.print(char(49+i));
     Serial.write(command); 
     Serial.write(GSM_CR);
     resOK = false;
-    timeout = millis() + 1000L; // ждём ответа 1 сек
+    timeout = millis() + 2000L; // ждём ответа 2 сек
     do {
       if (Serial.available()) {
         byte len = Serial.readBytesUntil('\n', buff, BUFF_SIZE);
@@ -166,13 +166,12 @@ bool performModem(const char* command, byte mode, byte retry, byte waitok) {
 }
 
 void connectModem() {
+  digitalWrite(PWR_LED_UNO, HIGH);
   delay(15000); 
   lcd.clear(); 
   lcd.setCursor(0,0);
   sprintf(buff, "sample %ld", MODEM_RATE);
   lcd.print(buff);
-
-  digitalWrite(PWR_LED_UNO, HIGH);
 
   Serial.begin(MODEM_RATE);
   delay(5000);
@@ -185,33 +184,17 @@ void connectModem() {
  
   lcd.setCursor(2,0);
   lcd.print(F("registration"));
-  while (!performModem("AT+CREG?", GSM_RG, 1, 1)) {
-/*
-    Serial.write("AT+COPS=4,2,\"25002\""); 
-    Serial.write(GSM_CR);
-    delay(5000);
-*/
-  } 
+//  Serial.write("AT+COPS=4,2,\"25002\""); Serial.write(GSM_CR); delay(5000);
+  while (!performModem("AT+CREG?", GSM_RG, 1, 1)) {} 
 
   lcd.setCursor(3,0);
   lcd.print(F("initial setup"));
-  while (!performModem("ATE0", GSM_OK, 1, 1)) {}
-  while (!performModem("AT+CLIP=1", GSM_OK, 1, 1)) {}
-  while (!performModem("AT+CMGF=1", GSM_OK, 1, 1)) {}
-  while (!performModem("AT+CSCS=\"GSM\"", GSM_OK, 1, 1)) {}
-/*
-  lcd.setCursor(3,0);
-  lcd.print("initial setup");
-  //performModem("AT+IPR=115200", GSM_OK, 1, 1); // Set fixed local rate
-  performModem("ATE0", GSM_OK, 1, 1);        // Set echo mode off 
-  performModem("AT+CLIP=1", GSM_OK, 1, 1);   // Set caller ID on
-  performModem("AT+CMGF=1", GSM_OK, 1, 1);   // Set SMS to text mode
-  performModem("AT+CSCS=\"GSM\"", GSM_OK, 1, 1); //  Character set of the mobile equipment
-  //performModem("AT&W", GSM_OK, 1, 1);        // Stores current configuration to user defined profile
-*/
+  while (!performModem("ATE0", GSM_OK, 1, 1)) {}            // Set echo mode off 
+  while (!performModem("AT+CLIP=1", GSM_OK, 1, 1)) {}       // Set caller ID on
+  while (!performModem("AT+CMGF=1", GSM_OK, 1, 1)) {}       // Set SMS to text mode
+  while (!performModem("AT+CSCS=\"GSM\"", GSM_OK, 1, 1)) {} // Character set of the mobile equipment
   lcd.setCursor(3,17);
   lcd.print(F("OK"));
-
 }
 
 void isCall() {
@@ -221,34 +204,37 @@ void isCall() {
   // +CLIP: "7XXXXXXXXXX",145,"",,"",0 
   // а если послать запрос AT+CLCC то модем выдаст строку типа
   // +CLCC: 1,1,4,0,0,"7XXXXXXXXXX",145
-  Serial.write("AT+CLCC"); 
-  Serial.write(GSM_CR);
   char eol = '\0';
   char* number = &eol;
   byte len;
-  bool allow;
-  timeout = millis() + 1000L; 
+  bool allow, first = true;
+
+  timeout = millis() + REPORT_INTERVAL; 
   do {
     if (Serial.available()) {
-      len = Serial.readBytesUntil('\n', buff, BUFF_SIZE - 1);
-      buff[len] = '\0';
-      if (len > 19 && buff[1] == 'C' && buff[2] == 'L' && buff[3] == 'I' && buff[4] == 'P') {
+      len = Serial.readBytesUntil('\n', buff, BUFF_SIZE);
+      if (first && len > 3 && buff[0] == 'R' && buff[1] == 'I' && buff[2] == 'N' && buff[3] == 'G') {
+        Serial.write("AT+CLCC"); 
+        Serial.write(GSM_CR);
+        first = false;
+      } else if (len > 19 && buff[1] == 'C' && buff[2] == 'L' && buff[3] == 'I' && buff[4] == 'P') {
         buff[19] = '\0';
         number = &buff[8];
+        lcd.setCursor(0,0); lcd.print(' ');
+        lcd.setCursor(0,1); lcd.print(number);
         break;
-      }
-      else if (len > 29 && buff[1] == 'C' && buff[2] == 'L' && buff[3] == 'C' && buff[4] == 'C') {
+      } else if (len > 29 && buff[1] == 'C' && buff[2] == 'L' && buff[3] == 'C' && buff[4] == 'C') {
         buff[29] = '\0';
         number = &buff[18];
+        lcd.setCursor(1,0); lcd.print(' ');
+        lcd.setCursor(1,1); lcd.print(number);
         break;
       }
     }
   } while (millis() < timeout);
 
   if (number[0] != '\0') {
-    delay(4000);
-    //Serial.write("ATH"); 
-    //Serial.write(GSM_CR); 
+    delay(5000);
     performModem("ATH", GSM_OK, 1, 1);
     for (byte i = 0; i < USERS_COUNT; i++ ) {
       allow = true;
@@ -256,25 +242,25 @@ void isCall() {
         allow = allow && number[j] == comrade[i][j+1];
       }
       if (allow) {
-        delay(4000);
+        lcd.setCursor(2,0); lcd.print(comrade[i]);
+        delay(5000);
         sprintf(buff, "AT+CMGS=\"%s\"", comrade[i]);
         if (performModem(buff, GSM_SM, 1, 1)) {
           for (byte i = 0; i <= connected; i++ ) {
             Serial.write(answer[i]); 
           }
           Serial.write(CTRL_Z); 
-          Serial.write(GSM_CR); 
-          Serial.write(GSM_CR); 
-          delay(5000);
         } else {
           Serial.write(ESCAPE); 
-          Serial.write(GSM_CR); 
-          Serial.write(GSM_CR); 
         }
+        Serial.write(GSM_CR); 
+        Serial.write(GSM_CR); 
+        delay(5000);
         break;
       }
     }
   }
+
 }
 
 // ==================== main =======================
@@ -299,57 +285,53 @@ void setup(void)
     delay(100);
   }
   connectModem();
-  reportTime = millis();
   measureTime = millis();
   messageTime = millis();
 }
 
 void loop(void) { 
 
-  isCall(); // timeout 1000L
+  isCall(); // timeout REPORT_INTERVAL
   
-  if (millis() > reportTime) {
-    digitalWrite(PWR_LED_UNO, LOW);
-    // замер температуры и формирование отчёта answer
-    getTemp(); 
-    // проверка модема
-    if (!performModem("AT", GSM_OK, 1, 1)) {
-      connectModem(); 
-    } 
-    getSignalQuality ();
+  digitalWrite(PWR_LED_UNO, LOW);
+  // замер температуры и формирование отчёта answer
+  getTemp(); // timeout 1 с
+  // проверка модема
+  if (!performModem("AT", GSM_OK, 1, 1)) {
+    connectModem(); 
+  } 
+  getSignalQuality ();
+  if (!digitalRead(PWR_KEY_UNO)) digitalWrite(PWR_LED_UNO, HIGH);
 
-    if (!digitalRead(PWR_KEY_UNO)) digitalWrite(PWR_LED_UNO, HIGH);
-    reportTime = millis() + REPORT_INTERVAL;
-  }
-
-  // почасовые замеры за последние сутки
   if (millis() > measureTime) {
-    // проверка регистрации 
+    // проверка регистрации в сети Мегафона
     creg1 = '?'; creg2 = '?'; creg3 = '?';
     if (!performModem("AT+CREG?", GSM_RG, 1, 1)) {
       connectModem(); 
     } 
- 
+    // запомним почасовые замеры за последние сутки
     for (byte s = 0; s < DEVICE_COUNT; s++) {
       for (byte t = 23; t > 0; t--) {
         hourlyTemperature[s][t] = hourlyTemperature[s][t-1];
       }
       hourlyTemperature[s][0] = currentTemperature[s];
      }
-    measureTime = millis() + MEASURE_INTERVAL;
+    measureTime += MEASURE_INTERVAL;
   }
 
   if (millis() > messageTime) {
-    if (performModem("AT+CMGS=\"+79219258698\"", GSM_SM, 1, 1)) {
+    if (performModem("AT+CMGS=\"+7ХХХХХХХХХХ\"", GSM_SM, 1, 1)) {
       for (byte i = 0; i <= connected; i++ ) {
         Serial.write(answer[i]); 
       }
       Serial.write(CTRL_Z); 
-      Serial.write(GSM_CR); 
-      Serial.write(GSM_CR); 
-      delay(3000);
+    } else {
+      Serial.write(ESCAPE); 
     }
-    messageTime = millis() + MESSAGE_INTERVAL;
+    Serial.write(GSM_CR); 
+    Serial.write(GSM_CR); 
+    delay(3000);
+    messageTime += MESSAGE_INTERVAL;
   }
 
 }
